5cf6589b4046f1171b0e3bc4c65b446587ae4294
vishnu vasita <vishnu@medianv.com>
2022-11-17 18:02:17 +0530

    update new fund amount

--- Routes/user_account.js
+++ Routes/user_account.js
@@ -5,87 +5,51 @@ const auth = require('../middleware/user_jwt')
 
 const user_account = require('../models/user_account')
 const { route } = require('./user')
+const user = require('../models/user')
 
 const router = express.Router()
 
-router.post('/register',async (req,res, next)=>{
-    const { name, email, number, password} = req.body;
-    try{
-        let userExist = await User.findOne({email: email});
-        if(userExist){
-            res.json({
-                success: false,
-                msg: 'user already exist'
-            });
-        }
-        let user = new User();
-        user.name= name;
-        user.email = email;
-        user.number= number;
 
-        const salt= await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(password, salt);
-
-
-        await user.save();
-
-        const payload ={
-            user: {
-                id: user.id
-            }
-        }
-        jwt.sign(payload, process.env.jwtUserToken, {
-            expiresIn: 400000
-        }, (err, token)=>{
-            if(err) throw err;
-            res.status(200).json({
-                success: true,
-                token: token
-            })
-        })
-
-        
-    }catch(err){
-        console.log(err)
-    }
-})
-
-
-
-
-
-
-
-
-
-
-//post balance of particular user
-router.post('/deposit', auth, async (req, res, next) => {
+//deposit fund
+router.put('/deposit/:id', async (req, res, next) => {
     try {
-        const user_detail = await user_account.create({
-            account_balance: req.body.account_balance,
-            user: req.user.id
-        });
 
-        if (!user_account) {
+        let user1 = await user_account.findById(req.params.id);
+        if (!user1) {
             return res.status(400).json({
                 success: false,
-                msg: 'Something went wrong'
-            });
-
+                msg: 'not exsists'
+            })
         }
 
+    
+        let newAmount = req.body.account_balance
+        const singleUserData = await user_account.findById(req.params.id)
+        const total= singleUserData.account_balance + newAmount
+        
+
+        user1 = await user_account.findByIdAndUpdate(req.params.id, { 'account_balance': total} ,
+            {
+                new: true,
+                runValidators: true
+            });
+
+    
 
         res.status(200).json({
             success: true,
-            user_account_details: user_detail,
-            msg: 'Done'
-        });
+            userDetails: user1,
+            msg: 'updated successfully'
+        })
+
     } catch (err) {
-        next(err);
+        next(err)
     }
+
 })
 
+
+//post balance of particular user
 
 
 //fetch user data
